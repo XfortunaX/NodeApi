c1fb314ca5f0ed8577cc0dc6259fe5625e9476db
XfortunaX <buklin.sergei@yandex.ru>
2017-06-12 09:58:27 +0300

    testt

--- queries.js
+++ queries.js
@@ -9,7 +9,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-pgp.pg.defaults.poolSize = 15;
+pgp.pg.defaults.poolSize = 25;
 var connectionString = "postgres://docker:docker@localhost:5432/docker";
 var db = pgp(connectionString);
 
@@ -437,6 +437,51 @@ function createVote(req, res, next) {
           res.status(404).send(err);
         });
     });
+
+
+
+  // db.one('select id from threads where ' + slug_or_id + ' = $1', slug)
+  //   .catch( err => {
+  //     res.status(404).send();
+  //   })
+  //   .then( data => {
+  //     threadId = data.id;
+  //     return db.one('select voice, id from votes where username = $1 and thread = $2', [nickname, threadId])
+  //   })
+  //   .then( data => {
+  //       deltaVoice = -(data.voice - voice);
+  //     db.tx(t => {
+  //       let q1 = t.none('update votes set (voice) = (' + voice + ') where id = $1', data.id);
+  //       let q2 = t.none('update threads set (votes) = (votes + ' + deltaVoice + ') where id = $1', threadId);
+  //       let q3 = t.one('select forums.slug as forum, threads.author, threads.created, threads.id,' +
+  //       ' threads.message, threads.slug, threads.title, threads.votes from threads inner join forums' +
+  //       ' on (threads.forum=forums.id) where threads.id = $1', threadId);
+  //     return t.batch([q1, q2, q3]);
+  //     })
+  //     .then( data => {
+  //         let d = JSON.stringify(data[2]);
+  //       d = JSON.parse(d);
+  //       res.status(200).send(d);
+  //     })
+  //   })
+  //   .catch( err => {
+  //     db.tx(t => {
+  //     let q1 = t.none('insert into votes (username, voice, thread) values ($1, $2, $3)', [nickname, voice, threadId]);
+  //     let q2 = t.none('update threads set (votes) = (votes + ' + voice + ') where id = $1', threadId);
+  //     let q3 = t.one('select forums.slug as forum, threads.author, threads.created, threads.id,' +
+  //     ' threads.message, threads.slug, threads.title, threads.votes from threads inner join forums' +
+  //     ' on (threads.forum=forums.id) where threads.id = $1', threadId);
+  //     return t.batch([q1, q2, q3]);
+  //     })
+  //     .then( data => {
+  //         let d = JSON.stringify(data[2]);
+  //       d = JSON.parse(d);
+  //       res.status(200).send(d);
+  //     })
+  //     .catch ( err => {
+  //       res.status(404).send(err);
+  //     });
+  //   })
 }
 
 function getThread(req, res, next) {
